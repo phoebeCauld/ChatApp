301fe3f85cd1abc45933eb2cf3f02d730d2d2463
phoebeCauld <vikaperova291013@gmail.com>
2022-01-02 12:59:56 +0300

    initial commit

---
+++ ChatApp/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by F1xTeoNtTsS on 02.01.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
